81f21240a45fd3d8155e71c6bdf4a5f9e00c7aea
horlali <gideon.ahiadzi@gmail.com>
2020-12-30 00:40:32 +0000

    Update app.py
    Updated with neew visualization

--- app.py
+++ app.py
@@ -16,142 +16,181 @@ import cufflinks as cf
 from plotly.offline import download_plotlyjs,plot,iplot
 cf.go_offline()
 
+def main():
+    
+    # and Navigation bar
+    st.sidebar.title("Accommodation in Accra: An Exploratory Data Analysis")
+    st.sidebar.markdown("This application is an Accommodation Price in Accra Dashboard")
 
-# and Navigation bar
-st.sidebar.title("Accommodation in Accra: An Exploratory Data Analysis")
-st.sidebar.markdown("This application is an Accommodation Price in Accra Dashboard")
+    # Title
+    st.title("Accommodation in Accra: An Exploratory Data Analysis")
+    st.header("This application is an Accommodation Price in Accra Dashboard")
 
-# Title
-st.title("Accommodation in Accra: An Exploratory Data Analysis")
-st.header("This application is an Accommodation Price in Accra Dashboard")
-
-st.title("Data Exploration")
-st.header("Explore the Dataset with the option below")
-
-
-# DataFrame
-dataset = 'accra_housing_dataset.xlsx'
+    st.title("Data Exploration")
+    st.header("Explore the Dataset with the option below")
 
 
-# Enabling Cache
-@st.cache(persist=True)
-
-# Function to Load Dataset
-def load_data(data):
-    df = pd.read_excel(os.path.join(dataset))
-    return df
-
-data = load_data(dataset)
+    # DataFrame
+    dataset = 'accra_housing_dataset.csv'
 
 
-# Showing heads and tails of Dataset
-show_data = st.radio("What section of the Dataset do you want to see",("Head","Tails","Sample","Whole"))
-if show_data == 'Head':
-    st.text("Showing the first 5 entries of the Dataset")
-    st.write(data.head())
-elif show_data == 'Tails':
-    st.text('Showing the last 5 entries of the Dataset')
-    st.write(data.tail())
-elif show_data == 'Sample':
-    st.text('Showing some 5 random entries from the Dataset')
-    st.write(data.sample(5))
-else:
-    st.text('Show entire Dataset')
-    st.dataframe(data)
+    # Enabling Cache
+    @st.cache(persist=True, show_spinner=True)
+
+    # Function to Load Dataset
+    def load_data(data):
+        df = pd.read_csv(os.path.join(dataset))
+        return df
+
+    data = load_data(dataset)
 
 
-# Show Column Names
-if st.checkbox("Show column Names"):
-    st.write(data.columns)
+    # Showing heads and tails of Dataset
+    show_data = st.radio("What section of the Dataset do you want to see",("Head","Tails","Sample","Whole"))
+    if show_data == 'Head':
+        st.text("Showing the first 5 entries of the Dataset")
+        st.write(data.head())
+    elif show_data == 'Tails':
+        st.text('Showing the last 5 entries of the Dataset')
+        st.write(data.tail())
+    elif show_data == 'Sample':
+        st.text('Showing some 5 random entries from the Dataset')
+        st.write(data.sample(5))
+    else:
+        st.text('Show entire Dataset')
+        st.dataframe(data)
 
 
-# Show Dimensions
-data_dim = st.radio("What Dimension do you want to see?", ("Rows","Columns","All"))
-if data_dim == "Rows":
-    st.text("Showing Rows")
-    st.write(data.shape[0])
-elif data_dim == "Columns":
-    st.text("Showing Colums")
-    st.write(data.shape[1]) 
-else:
-    st.text("Showing shape of Dataset")
-    st.write(data.shape)
+    # Show Column Names
+    if st.checkbox("Show column Names"):
+        st.write(data.columns)
 
 
-# Show Summary
-data_summary = st.radio("This show a summary of the Dataset, both Numeric and Categorical",("Numeric","Categorical"))
-if data_summary == "Numeric":
-    st.text("Show Summary of Numerical Columns in Dataset")
-    st.write(data.describe().transpose())
-else:
-    st.text("Show Summary of Categorical Columns in Dataset")
-    st.write(data.describe(exclude='number').transpose())
+    # Show Dimensions
+    data_dim = st.radio("What Dimension do you want to see?", ("Rows","Columns","All"))
+    if data_dim == "Rows":
+        st.text("Showing Rows")
+        st.write(data.shape[0])
+    elif data_dim == "Columns":
+        st.text("Showing Colums")
+        st.write(data.shape[1]) 
+    else:
+        st.text("Showing shape of Dataset")
+        st.write(data.shape)
 
 
-# Select A Column
-col_option = st.selectbox("Select Column",("price","categories","bedrooms",
-"bathrooms","garage","area","location","amenities","furnished"))
-if col_option == "price":
-    st.write(data['price'])
-elif col_option == "categories":
-    st.write(data['categories'])
-elif col_option == "bedrooms":
-    st.write(data['bedrooms'])
-elif col_option == "bathrooms":
-    st.write(data['bathrooms'])
-elif col_option == "garage":
-    st.write(data['garage'])
-elif col_option == "area":
-    st.write(data['area'])
-elif col_option == "location":
-    st.write(data['location'])
-elif col_option == "amenities":
-    st.write(data['amenities'])
-elif col_option == "furnished":
-    st.write(data['furnished'])
-else:
-    st.write("Select a Column to show dataset")
+    # Show Summary
+    data_summary = st.radio("This show a summary of the Dataset, both Numeric and Categorical",("Numeric","Categorical"))
+    if data_summary == "Numeric":
+        st.text("Show Summary of Numerical Columns in Dataset")
+        st.write(data.describe().transpose())
+    else:
+        st.text("Show Summary of Categorical Columns in Dataset")
+        st.write(data.describe(exclude='number').transpose())
 
 
+    # Select A Column
+    col_option = st.selectbox("Select Column",("price","categories","bedrooms",
+    "bathrooms","garage","area","location","amenities","furnished"))
+    if col_option == "price":
+        st.write(data['price'])
+    elif col_option == "categories":
+        st.write(data['categories'])
+    elif col_option == "bedrooms":
+        st.write(data['bedrooms'])
+    elif col_option == "bathrooms":
+        st.write(data['bathrooms'])
+    elif col_option == "garage":
+        st.write(data['garage'])
+    elif col_option == "area":
+        st.write(data['area'])
+    elif col_option == "location":
+        st.write(data['location'])
+    elif col_option == "amenities":
+        st.write(data['amenities'])
+    elif col_option == "furnished":
+        st.write(data['furnished'])
+    else:
+        st.write("Select a Column to show dataset")
 
 
-# Data Visualization
+    # Data Visualization
 
-st.title("Data Visualization")
-st.header("Explore this easy to understand interactive chart to understand accommodation pricing in Accra")
+    st.title("Data Visualization")
+    st.header("Explore this easy to understand interactive chart to understand accommodation pricing in Accra")
 
-# World Cloud of Location
-st.header("WordCloud of Location of Apartment Listing")
-from wordcloud import WordCloud
-texts = " ".join(location for location in data.location)
-wordcloud = WordCloud(width=3000,height=1800,margin=1,max_font_size=150).generate(texts)
+    # World Cloud of Location
+    st.header("WordCloud of Location of Apartment Listing")
+    from wordcloud import WordCloud
+    texts = " ".join(location for location in data.location)
+    wordcloud = WordCloud(width=3000,height=1800,margin=1,max_font_size=150).generate(texts)
 
-if st.checkbox("Word Cloud of Locations"):
-    fig, ax = plt.subplots()
-    st.write(plt.imshow(wordcloud))
-    st.write(plt.axis('off'))
-    st.pyplot(fig)
-
-
-# Distribution of Prices
-st.header("Distribution of Prices")
-dist_option = st.selectbox('Select option',["Boxplot","Histogram"])
-if dist_option == "Boxplot":
-    fig1 = data['price'].iplot(asFigure=True, kind='box',title='Distribution of Price')
-    st.plotly_chart(fig1)
-elif dist_option == "Histogram":
-    fig2 = data['price'].iplot(asFigure=True, kind='hist',bins=15,title='Distribution of Price')
-    st.plotly_chart(fig2)
+    if st.checkbox("Word Cloud of Locations"):
+        fig, ax = plt.subplots()
+        st.write(plt.imshow(wordcloud))
+        st.write(plt.axis('off'))
+        st.pyplot(fig)
 
 
-# A bubble plot of price and the number of bedrooms, with respect to floor area.
-st.header("Bubble Plot - [Price, Bedrooms, Area]")
-if st.checkbox("A bubble plot of price and the number of bedrooms, with respect to floor area."):
-    fig = data.iplot(asFigure=True, kind='bubble',x='bedrooms',y='price',size='area')
-    st.plotly_chart(fig)
+    # Distribution of Prices
+    st.header("Distribution of Prices")
+    dist_option = st.selectbox('Select option',["Boxplot","Histogram"])
+    if dist_option == "Boxplot":
+        fig1 = data['price'].iplot(asFigure=True, kind='box',title='Distribution of Price')
+        st.plotly_chart(fig1)
+    elif dist_option == "Histogram":
+        fig2 = data['price'].iplot(asFigure=True, kind='hist',bins=15,title='Distribution of Price')
+        st.plotly_chart(fig2)
 
 
-# Pairplot of the Distribution
-# Distribution of the Number of Bedrooms vs Price
-# Relationships between Price and other numerical variable.
-# Count of Categorical Variable
+    # A bubble plot of price and the number of bedrooms, with respect to floor area.
+    st.header("Bubble Plot - (Price, Bedrooms, Area)")
+    if st.checkbox("A bubble plot of price and the number of bedrooms, with respect to floor area."):
+        fig = data.iplot(asFigure=True, kind='bubble',x='bedrooms',y='price',size='area')
+        st.plotly_chart(fig)
+
+
+    # Pairplot of the Distribution
+    st.header("Pairplot of the Distribution")
+    if st.checkbox("Plot of the pairwise distribution of dataset"):
+        st.pyplot(sns.pairplot(data))
+
+
+    # Distribution of the Number of Bedrooms vs Price
+    st.header("Distribution of Number of Bedrooms and Price")
+    if st.checkbox("Distribution of Number of Bedrooms and Price"):
+        fig, axis = plt.subplots(1,1, figsize = (15,10))
+        sns.boxplot(data=data, x = 'bedrooms', y = 'price')
+        axis.set_title('bedrooms Vs Price' )
+        st.pyplot(fig)
+
+
+    # Relationships between Price and other numerical variable.
+    st.title("INSIGHT FROM DATA")
+    st.header("This section reveals some of the most useful insight from the Dataset")
+
+    st.markdown("Correlation Coefficient between Price, Floor Accra, Number of Bedrooms, etc.")
+    st.write(data.corr())
+    
+    st.header("Visualization of Insights")
+    # Visualization of Insights
+    st.subheader("Relationship Between Price and Numerical Columns")
+    insight_vs = st.selectbox("Select a feature to show relationship", ["Price vs Bedrooms", "Price vs Floor Area","Price vs Bathrooms","Price vs Garage"])
+    if insight_vs == "Price vs Bedrooms":
+        figure = data.iplot(kind='scatter',x='price',y='bedrooms',mode='markers',xTitle='Price per month',yTitle='No. of Bedrooms')
+        st.plotly_chart(figure)
+    ''' elif insight_vs == "Price vs Floor Area":
+        figure = df.iplot(kind='scatter',x='price',y='area',mode='markers',xTitle='Price per month',yTitle='Floor Area')
+        st.plotly_chart(figure)
+    elif insight_vs == "Price vs Bathrooms":
+        figure = df.iplot(kind='scatter',x='price',y='bathrooms',mode='markers',xTitle='Price per month',yTitle='No. of Bathrooms')
+        st.plotly_chart(figure)
+    else:
+        figure = df.iplot(kind='scatter',x='price',y='garage',mode='markers',xTitle='Price per month',yTitle='No. of Garage')
+        st.plotly_chart(figure)
+    '''
+
+    # Count of Categorical Variable
+
+if __name__ == "__main__":
+    main()
